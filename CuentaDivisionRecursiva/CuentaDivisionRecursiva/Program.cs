db394ba8f17f72353eea2412b064c8bddb9a2ba7
Nyoseka <47579115+Nyoseka@users.noreply.github.com>
2020-03-14 11:16:39 -0700

    Carga 1

---
+++ CuentaDivisionRecursiva/CuentaDivisionRecursiva/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuentaDivisionRecursiva
+{
+    class Program
+    {
+        public static int CuentaDivisiones(double num)
+        {
+            int count = 0;
+            if(num>0 && num%2==0)
+            {
+                count++;
+                num /= 2;
+                return count += CuentaDivisiones(num);
+            }
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Captura número: ");
+            double numero = Convert.ToDouble(Console.ReadLine());
+            int count = CuentaDivisiones(numero);
+            Console.WriteLine($"Total de divisiones: {count}");
+            Console.ReadKey();
+        }
+    }
+}
