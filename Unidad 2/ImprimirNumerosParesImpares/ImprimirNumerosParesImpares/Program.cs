487f2fb056b73312e458810e571329988bce8fa0
Nyoseka <47579115+Nyoseka@users.noreply.github.com>
2020-08-10 12:08:03 -0700

    Recursivvidad Añadida

---
+++ Unidad 2/ImprimirNumerosParesImpares/ImprimirNumerosParesImpares/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImprimirNumerosParesImpares
+{
+    class Program
+    {
+        public static int ParImpar(int val, int n)
+        {
+            if(val>n)
+            {
+                return val;
+            }
+            else
+            {
+                Console.Write(val + " ");
+                return ParImpar(val + 2, n);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int rango;
+            Console.Write("Indique el rango de números que desea imprimir: ");
+            rango = int.Parse(Console.ReadLine());
+            Console.WriteLine("NÚMEROS PARES");
+            ParImpar(2, rango);
+            Console.WriteLine('\n');
+            Console.WriteLine("NÚMEROS IMPARES");
+            ParImpar(1, rango);
+            Console.ReadKey();
+        }
+    }
+}
