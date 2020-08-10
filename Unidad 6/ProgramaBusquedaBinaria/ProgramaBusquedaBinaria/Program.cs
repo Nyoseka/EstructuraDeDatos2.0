781e36bb764b327991b8c578347441356e337b06
Nyoseka <47579115+Nyoseka@users.noreply.github.com>
2020-08-10 12:06:53 -0700

    Busquedas Añadidas

---
+++ Unidad 6/ProgramaBusquedaBinaria/ProgramaBusquedaBinaria/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramaBusquedaBinaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string imput = Console.ReadLine();
+                int value_of_imput = int.Parse(imput);
+                int min = 0;
+                int max = 20;
+                int[] binary_search = new int[value_of_imput];
+                Random randNum = new Random();
+                for (int i = 0; i < value_of_imput; i++)
+                {
+                    binary_search[i] = randNum.Next(min, max);
+                    Console.Write(binary_search[i] + " ");
+                }
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.Write("Ahora ingrese el número entero de esta matriz para realziar la busqueda binaria: ");
+                string get_search = Console.ReadLine();
+                int value_of_get_search = int.Parse(get_search);
+                Array.Sort(binary_search);
+                Console.WriteLine("");
+                Console.WriteLine("");
+                for (int i = 0; i < value_of_imput; i++)
+                {
+                    Console.Write(binary_search[i] + " ");
+                }
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("Ahora busquemos su valor entero...");
+                int get_middle = 0;
+                int low = 0;
+                int high = (binary_search.Length) - 1;
+                int mid = (low + high) / 2;
+                int track_middle = 0;
+                while (low <= high)
+                {
+                    mid = (low + high) / 2;
+                    get_middle = binary_search[mid];
+                    if (get_middle == value_of_get_search)
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("Encontró su número entero: " + value_of_get_search);
+                        break;
+                    }
+                    if (get_middle > value_of_get_search && get_middle != track_middle)
+                    {
+                        high = mid + 1;
+                        Console.WriteLine("");
+                        Console.WriteLine("Encontré este entero: " + get_middle + " Pero eso no es todo");
+                        track_middle = get_middle;
+                    }
+                    else
+                    {
+                        low = mid - 1;
+                        Console.WriteLine("Encontré este entero: " + get_middle + " Pero eso no es todo");
+                    }
+                }
+                if (low > high)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("No se puede encontrar este valor, intentelo de nuevo");
+                }
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("Presione cualquier tecla para continuar");
+                Console.ReadKey(true);
+            }
+            catch
+            {
+                Console.WriteLine("Ingrese un número e intente de nuevo");
+                Console.ReadKey(true);
+            }
+            
+        }
+            
+    }
+}
---
+++ Unidad 6/ProgramaBusquedaLineal/ProgramaBusquedaLineal/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramaBusquedaLineal
+{
+    class Program
+    {
+        public static int LinearSearch(ref int[] x, int valueToFind)
+        {
+            for(int i=0;i<x.Length;i++)
+            {
+                if(valueToFind==x[i])
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public static void DisplayElements(ref int[] xArray,char status, string sortname)
+        {
+            if (status == 'a')
+            {
+                Console.WriteLine("Después de ordenar usando el algoritmo: " + sortname);
+            }
+            else
+            {
+                Console.WriteLine("Antes de ordenar");
+                for(int i=0;i<=xArray.Length-1;i++)
+                {
+                    if ((i != 0) && (i % 10 == 0))
+                    {
+                        Console.Write('\n');
+                        Console.Write(xArray[i] + " ");
+                    }
+                }
+                Console.ReadLine();
+            }
+        }
+        static void MixDataUp(ref int[]x,Random rdn)
+        {
+            for (int i = 0; i <=x.Length - 1; i++)
+            {
+                x[i] = (int)(rdn.NextDouble() * x.Length);
+            }
+        }
+        static void Main(string[] args)
+        {
+            const int nItems = 20;
+            Random rdn = new Random(nItems);
+            int[] xdata = new int[nItems];
+            MixDataUp(ref xdata, rdn);
+            DisplayElements(ref xdata, 'b', "");
+            Console.WriteLine("Utilizando el algoritmo de búsqueda lineal para ver el 4to elemento de la lista aleatoria");
+            int location = LinearSearch(ref xdata, xdata[4]);
+            if (location == -1)
+            {
+                Console.WriteLine("El elemento no fue encontrado en la lista");
+            }
+            else
+            {
+                Console.WriteLine("Encontrado en la ubicación= {0}", location);
+            }
+            location = LinearSearch(ref xdata, 19);
+            if(location==-1)
+            {
+                Console.WriteLine("El valor 19 no fue encontrado en la lista");
+            }
+            else
+            {
+                Console.WriteLine("El valor 19 fue encontrado en la ubicación = {0}", location);
+            }
+            Console.WriteLine("\n\n");
+        }
+    }
+}
