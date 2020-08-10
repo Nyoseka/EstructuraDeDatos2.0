31355e1a924909e0db18cdc27eb37f0d9dbcd511
Nyoseka <47579115+Nyoseka@users.noreply.github.com>
2020-08-10 12:07:40 -0700

    Ordenamientos Añadidos

---
+++ Unidad 5/ProgramaRadix/ProgramaRadix/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramaRadix
+{
+    class Program
+    {
+        static void Sort(int [] arr)
+        {
+            int i, j;
+            int[] temp = new int[arr.Length];
+            for(int shift=31; shift>-1;--shift)
+            {
+                j = 0;
+                for (i = 0; i < arr.Length; i++)
+                {
+                    bool move = (arr[i] << shift) >= 0;
+                    if (shift == 0 ? !move : move)
+                    {
+                        arr[i - j] = arr[i];
+                    }
+                    else
+                    {
+                        temp[j++] = arr[i];
+                    }
+                    Array.Copy(temp, 0, arr, arr.Length - j, j);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 2, 5, -4, 11, 018, 22, 67, 51, 6 };
+            Console.WriteLine("\nArreglo Original\n");
+            foreach(var item in arr)
+            {
+                Console.Write(" " + item);
+            }
+            Sort(arr);
+            Console.WriteLine("\nArreglo Ordenado\n");
+            foreach (var item in arr)
+            {
+                Console.Write(" " + item);
+            }
+            Console.WriteLine('\n');
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Unidad 5/ProgramaShellsort/ProgramaShellsort/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramaShellsort
+{
+    public class Shellsort
+    {
+        public void shellSort(int[] arr,int array_size)
+        {
+            int i, j, inc, temp;
+            inc = 3;
+            while(inc>0)
+            {
+                for(i=0; i <array_size;i++)
+                {
+                    j = i;
+                    temp = arr[i];
+                    while((j>=inc)&&(arr[j-inc] >temp))
+                    {
+                        arr[j] = arr[j - inc];
+                        j = j - inc;
+                    }
+                    arr[j] = temp;
+                }
+                if(inc/2!=0)
+                {
+                    inc = inc / 2;
+                }
+                else if(inc==1)
+                {
+                    inc = 0;
+                }
+                else
+                {
+                    inc = 1;
+                }
+            }
+        }
+        public void Show_Array(int[]arr)
+        {
+            foreach(var element in arr)
+            {
+                Console.Write(element + " ");
+            }
+            Console.Write('\n');
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 5, -4, 11, 0, 18, 22, 67, 51, 6 };
+            int n;
+            Shellsort ss = new Shellsort();
+            n = arr.Length;
+            Console.WriteLine("Elementos originales del arreglo: ");
+            ss.Show_Array(arr);
+            ss.shellSort(arr, n);
+            Console.WriteLine("\nElementos del arreglo ordenado: ");
+            ss.Show_Array(arr);
+            Console.ReadKey();
+        }
+    }
+}
